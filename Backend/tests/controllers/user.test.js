8f6eccdf1623b05e2de1273e671cd057b80750e3
Raman0925 <ramankumarkannaujiya@gmail.com>
2025-01-01 18:58:06 +0530

    Added test for User Model

--- Backend/tests/controllers/user.test.js
+++ Backend/tests/controllers/user.test.js
@@ -8,21 +8,44 @@ describe("Creating document in MongoDB", () => {
     try {
       const user = new User({
         fullName: "Raman",
-        emailId: "raman.k126306@gmail.com",
+        emailId: "raman.k6@gmail.com",
         password: "Raman0925@",
       });
-      
+
       expect(user.isNew).to.equal(true);
 
       await user.save();
-
       const savedUser = await User.findById(user._id);
       expect(savedUser).to.not.be.null;
-      
       
     } catch (error) {
       console.error("Test failed with error:", error);
       expect.fail(error.message);
     }
-  }).timeout(10000);  
+  }).timeout(10000);
+});
+
+describe("Stubbing the tests for creating the documents in mongodb", () => {
+  let saveStub;
+  const user = new User({
+    fullName: "Raman",
+    emailId: "raman.k12306@gmail.com",
+    password: "Raman0925@",
+  });
+  beforeEach(() => {
+    saveStub = sinon.stub(User.prototype, "save");
+  });
+  afterEach(()=>{
+    saveStub.restore();
+  })
+  it('should save the user',async()=>{
+    const mockUser = {
+        fullName: "Raman",
+        emailId: "raman.k12306@gmail.com",
+        password: "Raman0925@",
+      }
+      saveStub.resolves(mockUser)
+      const result = await user.save();
+      expect(result).to.deep.equal(mockUser)
+  })
 });
